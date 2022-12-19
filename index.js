63633f7ddda2fb3efd5d69722ad8da7547fa5782
Mikhail Mudrogelov <mudrogelov.beled@gmail.com>
2022-12-19 22:42:43 +0400

    fix: exit operation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import process from 'process';
 import { homedir } from 'os';
 import { createInterface } from 'readline';
 import { greeting, logCurrentPath, exit, getInputConvert } from './utils/index.js';
@@ -14,81 +15,86 @@ readline.on('line', (input) => {
 
   switch (inputConvert[0]) {
     case 'up':
-      if(inputConvert.length === 1) {
+      if (inputConvert.length === 1) {
         up();
         break;
       }
 
     case 'cd':
-      if(inputConvert.length === 2) {
+      if (inputConvert.length === 2) {
         cd(inputConvert[1]);
         break;
       }
 
     case 'ls': 
-      if(inputConvert.length === 1) {
+      if (inputConvert.length === 1) {
         ls();
         break;
       }
 
     case 'cat':
-      if(inputConvert.length === 2) {
+      if (inputConvert.length === 2) {
         cat(inputConvert[1]);
         break;
       }
       
     case 'add':
-      if(inputConvert.length === 2) {
+      if (inputConvert.length === 2) {
         add(inputConvert[1]);
         break;
       }
 
     case 'rn':
-      if(inputConvert.length === 3) {
+      if (inputConvert.length === 3) {
         rn(inputConvert[1], inputConvert[2]);
         break;
       }
 
     case 'cp':
-      if(inputConvert.length === 3) {
+      if (inputConvert.length === 3) {
         cp(inputConvert[1], inputConvert[2]);
         break;
       }
 
     case 'mv':
-      if(inputConvert.length === 3) {
+      if (inputConvert.length === 3) {
         mv(inputConvert[1], inputConvert[2]);
         break;
       }
 
     case 'rm':
-      if(inputConvert.length === 2) {
+      if (inputConvert.length === 2) {
         remove(inputConvert[1]);
         break;
       }
 
     case 'os':
-      if(inputConvert.length === 2) {
+      if (inputConvert.length === 2) {
         os(inputConvert[1]);
         break;
       }
 
     case 'hash':
-      if(inputConvert.length === 2) {
+      if (inputConvert.length === 2) {
         hash(inputConvert[1]);
         break;
       }
 
     case 'compress':
-      if(inputConvert.length === 3) {
+      if (inputConvert.length === 3) {
         compress(inputConvert[1], inputConvert[2]);
         break;
       }
 
     case 'decompress':
-      if(inputConvert.length === 3) {
+      if (inputConvert.length === 3) {
         decompress(inputConvert[1], inputConvert[2]);
         break;
+      }
+    
+    case '.exit':
+      if (inputConvert.length === 1) {
+        exit();
       }
 
     default:
@@ -97,4 +103,6 @@ readline.on('line', (input) => {
   }
 })
 
-exit();
+readline.on('close', () => {
+  exit();
+});
--- utils/exit.js
+++ utils/exit.js
@@ -1,8 +1,6 @@
-import process from 'node:process';
 import { getUsername } from './getUsername.js';
 
 export function exit() {
-  process.on('exit', () => {
-    console.log(`Thank you for using File Manager, ${getUsername()}, goodbye!`)
-  })
+  console.log(`Thank you for using File Manager, ${getUsername()}, goodbye!`);
+  process.exit();
 };
